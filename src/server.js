d4e0e11aa1f73d137ea49d51d88483433be5f7e2
Karel <karel.fischbein@st.oth-regensburg.de>
2022-06-28 15:20:31 +0200

    setup nodejs hapi with packages, modules and file structur

---
+++ src/server.js
@@ -0,0 +1,51 @@
+import Hapi from "@hapi/hapi";
+import Inert from "@hapi/inert";
+import Cookie from "@hapi/cookie";
+
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+import { accountsController } from "./controllers/accounts-controller.js";
+import { db } from "./models/db.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+}
+
+async function init() {
+  const server = Hapi.server({
+    port: process.env.PORT || 4000,
+    routes: { cors: true },
+  });
+
+  await server.register(Inert);
+  await server.register(Cookie);
+
+
+  server.auth.strategy("session", "cookie", {
+    cookie: {
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
+      isSecure: false,
+    },
+    redirectTo: "/",
+    validateFunc: accountsController.validate,
+  });
+  server.auth.default("session");
+
+  db.init("mongo");
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+await init();
